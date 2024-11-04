40f8c4d2fb910da219314d300730080972bf2779
billmeyer <bill.meyer@datadoghq.com>
2024-11-04 15:40:59 -0600

    Initial revision

---
+++ start.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+[[ -f ".env" ]] && source ".env"
+
+if [ "${#DD_API_KEY}" -ne 32 ]; then
+	echo "Environment variable DD_API_KEY not valid."
+	exit -1
+fi
+
+if [[ -z "${DD_SITE_PARAMETER}" ]]; then
+	echo "Environment variable DD_SITE_PARAMETER not set."
+	exit -1
+fi
+
+python3 -m venv venv
+source venv/bin/activate
+pip3 install -r requirements.txt
+jupyter lab
+
